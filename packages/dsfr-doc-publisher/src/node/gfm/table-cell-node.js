28d5bcf838fd01c0bd768b1d2ac0eff38a9c18ef
Baptiste.z <baptiste.zeller@actimage.com>
2024-12-09 17:30:08 +0100

    Update packages/dsfr-doc-publisher/src/node/gfm/table-cell-node.js
    Co-authored-by: lab9 <lab9.fr@gmail.com>

--- packages/dsfr-doc-publisher/src/node/gfm/table-cell-node.js
+++ packages/dsfr-doc-publisher/src/node/gfm/table-cell-node.js
@@ -2,7 +2,7 @@ import { Node } from '../node.js';
 
 class TableCellNode extends Node {
   constructor (data) {
-    const level = data.level && data.level === 'thead' ? 'th' : 'td';
+    const level = data?.level === 'thead' ? 'th' : 'td';
     super(data, level);
     if (this.data.align && this.data.align != 'left') this.attributes.setAttribute('class', `fr-cell--${this.data.align}`);
   }
