2c47b8ea7b2888b9298412abd0d20616870505b0
zellerbaptiste <zellerbaptiste@gmail.com>
2024-12-10 11:36:00 +0100

    feat(markdown): met a jour la template table-node

--- packages/dsfr-doc-publisher/src/node/gfm/table-node.js
+++ packages/dsfr-doc-publisher/src/node/gfm/table-node.js
@@ -2,28 +2,21 @@ import { Node } from '../node.js';
 
 class TableNode extends Node {
   constructor (data) {
-    data.children = data.children.map((child, index) => {
-      if (index === 0) child.level = 'thead';
-      child.align = data.align && data.align.length ? data.align : null;
-      return child;
-    });
+    data.children = data.children.map((child, index) => ({
+      ...child,
+      level: index === 0 ? 'thead' : undefined,
+      align: data?.align || null
+    }));
     super(data, `table`);
   }
 
   async render () {
-    let html = '';
-    for (const child of this._children) {
-      html += await child.render();
-    }
-
     return `
       <div class="fr-table">
         <div class="fr-table__wrapper">
           <div class="fr-table__container">
             <div class="fr-table__content">
-              <${this.tagName}${this.renderAttributes()}>
-                ${html}
-              </${this.tagName}>
+              ${await super.render()}
             </div>
           </div>
         </div>
