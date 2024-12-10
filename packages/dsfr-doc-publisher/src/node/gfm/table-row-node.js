db34d21e27161fe50eb6c8ebdbff0179b54fde59
zellerbaptiste <zellerbaptiste@gmail.com>
2024-12-10 12:16:45 +0100

    feat(markdown): met a jour la template table-row-node

--- packages/dsfr-doc-publisher/src/node/gfm/table-row-node.js
+++ packages/dsfr-doc-publisher/src/node/gfm/table-row-node.js
@@ -2,11 +2,11 @@ import { Node } from '../node.js';
 
 class TableRowNode extends Node {
   constructor (data) {
-    data.children = data.children.map((child, index) => {
-      child.level = data.level || null;
-      child.align = data.align && data.align.length ? data.align[index] : null;
-      return child;
-    });
+    data.children = data.children.map((child, index) => ({
+      ...child,
+      level: data.level || undefined,
+      align: data?.align ? data.align[index] : null
+    }));
     super(data, `tr`);
   }
 }
