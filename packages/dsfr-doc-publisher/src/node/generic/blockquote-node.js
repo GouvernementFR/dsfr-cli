e2eeeb17afbfaec5298469fbceedd8959741733f
zellerbaptiste <zellerbaptiste@gmail.com>
2024-12-11 17:55:28 +0100

    feat(markdown): rendu du composant callout

--- packages/dsfr-doc-publisher/src/node/generic/blockquote-node.js
+++ packages/dsfr-doc-publisher/src/node/generic/blockquote-node.js
@@ -2,31 +2,32 @@ import { Node } from '../node.js';
 
 class BlockquoteNode extends Node {
   constructor (data) {
+    if (data?.children?.[0]?.type === 'heading') data.children[0].attributes = { class: 'fr-callout__title' };
     super(data, 'div');
-    this.attributes.addClass('fr-alert fr-mb-6v');
+    this.attributes.addClass('fr-callout fr-mb-6v');
 
     switch (true) {
       case !data.level:
         break;
 
       case data.level === 'note':
-        this.attributes.addClass('fr-alert--info');
+        this.attributes.addClass('fr-icon-information-line fr-callout--blue-cumulus');
         break;
 
       case data.level === 'warning':
-        this.attributes.addClass('fr-alert--warning');
+        this.attributes.addClass('fr-icon-alert-line fr-callout--yellow-moutarde');
         break;
 
       case data.level === 'caution':
-        this.attributes.addClass('fr-alert--error');
+        this.attributes.addClass('fr-icon-error-warning-line fr-callout--pink-tuile');
         break;
 
       case data.level === 'tip':
-        this.attributes.addClass('fr-alert--success');
+        this.attributes.addClass('fr-icon-lightbulb-line fr-callout--green-emeraude');
         break;
 
       case data.level === 'important':
-        this.attributes.addClass('fr-alert--warning');
+        this.attributes.addClass('fr-icon-feedback-line fr-callout--purple-glycine');
         break;
 
       default:
